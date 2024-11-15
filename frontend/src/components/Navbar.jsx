c95654312e33f19b5d327bf0f73c333c0790c397
Utkarsh Ukani <utkarshukani@gmail.com>
2024-11-15 18:25:34 +0530

    add navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,106 @@
+import { Link } from "react-router-dom";
+import { GiBookshelf } from "react-icons/gi";
+import { IoSearch } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+import { FaRegHeart } from "react-icons/fa";
+import { TiShoppingCart } from "react-icons/ti";
+import avatarImg from "../assets/avatar.png";
+import { useState } from "react";
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Orders",
+    href: "/orders",
+  },
+  {
+    name: "Cart Page",
+    href: "/cart",
+  },
+  {
+    name: "Check Out",
+    href: "/checkout",
+  },
+];
+
+const Navbar = () => {
+  const currUser = true;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        {/* left side */}
+        <div className="flex items-center md:gap-16 gap-4">
+          <Link to="/">
+            <GiBookshelf className="size-6" />
+            BookStak
+          </Link>
+
+          {/* search input */}
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <IoSearch className="absolute inline-block left-4 inset-y-2" />
+            <input
+              type="text"
+              placeholder="Search here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          <div>
+            {currUser ? (
+              <>
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  <img
+                    src={avatarImg}
+                    alt="avt"
+                    className={`size-7 mt-1 rounded-full ${
+                      currUser ? "ring-2 ring-blue-500" : ""
+                    }`}
+                  />
+                </button>
+                {/* show dropdowns */}
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
+                      {navigation.map((item) => (
+                        <li key={item.name} onClick={()=>setIsDropdownOpen(false)} >
+                          <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-100">
+                            {item.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Link to="/login">
+                <HiOutlineUser className="size-6" />
+              </Link>
+            )}
+          </div>
+
+          <button className="hidden sm:block">
+            <FaRegHeart className="size-6" />
+          </button>
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm"
+          >
+            <TiShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
