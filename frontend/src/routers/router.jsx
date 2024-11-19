37c3a437148a541208edcf8883c4baf4c886cddc
Utkarsh Ukani <utkarshukani@gmail.com>
2024-11-19 15:49:19 +0530

    add private routes

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -6,46 +6,51 @@ import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
 import Checkout from "../pages/books/Checkout";
 import SingleBook from "../pages/books/SingleBook";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <App/>,
-        children:[
-            {
-                path:"/",
-                element:<Home/>
-            },
-            {
-                path:"/orders",
-                element: <div>Orders</div>
-            },
-            {
-                path:"/about",
-                element: <div>about</div>
-            },
-            {
-                path:"/login",
-                element: <Login/>
-            },
-            {
-                path:"/register",
-                element: <Register/>
-            },
-            {
-                path:"/cart",
-                element: <CartPage/>
-            },
-            {
-                path:"/checkout",
-                element: <Checkout/>
-            },
-            {
-                path:"/books/:id",
-                element: <SingleBook/>
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/orders",
+        element: <div>Orders</div>,
+      },
+      {
+        path: "/about",
+        element: <div>about</div>,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+      {
+        path: "/checkout",
+        element: (
+          <PrivateRoute>
+            <Checkout />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/books/:id",
+        element: <SingleBook />,
+      },
+    ],
+  },
+]);
 
 export default router;
